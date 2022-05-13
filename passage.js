eb335b2d2f7eb75b79cce87881b2f780816cd52e
Winry <winry.tian@gmail.com>
2022-05-13 11:59:46 -0700

    update passage message, add char wrapping, add line wrapping

--- passage.js
+++ passage.js
@@ -1,8 +1,13 @@
+// stops error generation for constants I don't use. I can choose to take it
+// away if I really want to check.
+
+// noinspection JSUnusedGlobalSymbols,JSUnusedLocalSymbols
+
 class Passage {
     constructor(text) {
-        // the passage text
-        this.text = text
-        // the index of the character we're typing in our passage
+        // the passage text, plus a space at the end to stop a bug in word wrap.
+        this.text = text + " "
+        // the index of the character we're typing in our passage. Not yet used.
         this.index = 0
         // a list of boolean values marking if the character was correct
         this.correctList = []
@@ -13,22 +18,75 @@ class Passage {
         this.TOP_MARGIN = 50
 
         // the x-position of the bounding box
-        this.SIDE_MARGIN = 50
+        this.X_MARGIN = 50
 
         // the place where the text starts relative to the bounding box
         this.TEXT_START_X = 50
 
+        // the distance from the top of the bounding box to the tallest
+        // character in the current font, which is consola.
+        this.DIST_FROM_BOX_TOP = 100 - textAscent() - this.TOP_MARGIN
+
+        // the position of x where we wrap the line. Ziatora's card's width
+        // is around 480 pixels, but I'm probably going to scale it down by
+        // a factor of 2. That means I need 240 pixels of space. I'd like
+        // another 80 just so that I can have a border of 40 pixels around
+        // Ziatora's image and then have that be where the text wraps.
+        this.LINE_WRAP_X_POS = width - 320
+
         // this is where the text starts. Ideally I'd like 50 pixels of space.
         this.TEXT_START = new p5.Vector(
-            this.TOP_MARGIN - textAscent(),
-            this.SIDE_MARGIN + this.TEXT_START_X
+            this.X_MARGIN + this.TEXT_START_X,
+            this.TOP_MARGIN + this.DIST_FROM_BOX_TOP
         )
+
+        // the spacing between lines, where the constant is an extra buffer.
+        this.DIST_BETWEEN_LINES = 10 + textAscent() + textDescent()
     }
 
 
     // displays the passage
     render() {
+        // set the font size
+        textSize(24)
 
+        // this is where our text will be drawn. It can always be modified.
+        let cursor = this.TEXT_START.copy()
+
+        for (let i = 0; i < this.text.length; i++) {
+            // retrieve our current character and display it
+            let currentChar = this.text[i]
+
+            // if the current letter will go past our line wrap x position,
+            // we need to wrap our text around. Currently, my program is
+            // incapable of retaining words and spacing. Later, I'll fix that.
+            // if (textWidth(currentChar) + cursor.x >= this.LINE_WRAP_X_POS) {
+            //     this.#wrapCursor(cursor)
+            // } // This is character wrap. Now I'll work on word wrap.
+
+            text(currentChar, cursor.x, cursor.y)
+
+            // if the current letter is a space, we can find the next space.
+            if (currentChar === " ") {
+                // if we don't increment i by one, this will return i
+                let nextSpace = this.text.indexOf(" ", i + 1)
+
+                // find the substring from our current delimiter to the next
+                let currentWord = this.text.substring(
+                    i, nextSpace
+                )
+
+                if (textWidth(currentWord) + cursor.x >= this.LINE_WRAP_X_POS) {
+                    this.#wrapCursor(cursor)
+
+                    // prevents extra unnecessary wrapping
+                    continue
+                }
+            }
+
+            // increment our cursor's x-position.
+            cursor.x += textWidth(currentChar)
+        }
     }
 
 
@@ -38,7 +96,7 @@ class Passage {
     }
 
 
-    // return whatever index we're at
+    // return whatever character we're at based on our index
     getCurrentChar() {
 
     }
@@ -64,9 +122,11 @@ class Passage {
 
     /* from here, there will only be private methods */
 
-    // increase y, reset x
-    #wrapCursor() {
 
+    // increase y, reset x
+    #wrapCursor(cursor) {
+        cursor.x = this.TEXT_START.x
+        cursor.y += this.DIST_BETWEEN_LINES
     }
 
 
--- sketch.js
+++ sketch.js
@@ -15,9 +15,8 @@ function preload() {
 }
 
 
-
 function setup() {
-    let cnv = createCanvas(600, 300)
+    let cnv = createCanvas(900, 600)
     cnv.parent('#canvas')
     colorMode(HSB, 360, 100, 100, 100)
     textFont(font, 14)
@@ -28,12 +27,11 @@ function setup() {
         [1,2,3,4,5] → no function
         z → freeze sketch</pre>`)
 
-    passage = new Passage("hi!")
-
     for (let i = 0; i < Object.keys(scryfall["data"]).length; i++) {
         // a string of data that contains scryfall data.
         let typingText = scryfall["data"][i].name
         typingText += " " + scryfall["data"][i].mana_cost
+        typingText += "\n" + scryfall["data"][i].type_line
         typingText += "\n" + scryfall["data"][i].oracle_text
 
         if (scryfall["data"][i].flavor_text !== undefined) {
@@ -42,20 +40,24 @@ function setup() {
 
         if (scryfall["data"][i].power !== undefined &&
             scryfall["data"][i].toughness !== undefined) {
-            typingText += "\n" + scryfall["data"][i].power + "/" + scryfall["data"][i].toughness
+            typingText += "\n" + scryfall["data"][i].power
+            typingText += "/" + scryfall["data"][i].toughness
         }
 
-        print(typingText)
-
-        // switch between border_crop and art_crop for different aspects of art.
-        // print(`${scryfall["data"][i].name} ${scryfall["data"][i]["image_uris"].border_crop}`)
+        // print(typingText)
     }
+
+    passage = new Passage("This unit's line wrapping works. This is supposed" +
+        " to be a test message. Currently, typing is not supported, but you" +
+        " will still see this message displayed. In my Github repository," +
+        " this will also display, and with proper technology you may be" +
+        " able to run this program. End of message, W-1000")
 }
 
 
 function draw() {
     background(234, 34, 24)
-
+    passage.render()
 
     displayDebugCorner()
 }
@@ -63,6 +65,7 @@ function draw() {
 
 /** 🧹 shows debugging info using text() 🧹 */
 function displayDebugCorner() {
+    textSize(14)
     const LEFT_MARGIN = 10
     const DEBUG_Y_OFFSET = height - 10 /* floor of debug corner */
     const LINE_SPACING = 2
